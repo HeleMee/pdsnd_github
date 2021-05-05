7c0d9e98d9a7241b30355bf98e5ee0e9d209b454
Your-Full-Name <your-email-address>
2021-05-05 11:25:36 +0200

    improve description of load_data function

--- bikeshare_project.py
+++ bikeshare_project.py
@@ -54,7 +54,7 @@ def get_filters():
 
 def load_data(city, month, day_of_week):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city and filters by month and day of the week if applicable.
 
     Args:
         (str) city - name of the city to analyze
